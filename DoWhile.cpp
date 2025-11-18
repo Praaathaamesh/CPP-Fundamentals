88910d1239e9296258959d2ea6b5ff2be18479cf
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-18 21:50:32 +0530

    Feat: Add Do While loop code
    Add Do while loop code in C++

---
+++ DoWhile.cpp
@@ -0,0 +1,17 @@
+// Following is the code for do while loop
+/*
+ Do this code
+ then check the while condition
+ */
+
+int main()
+{
+	using namespace std;
+	int Number;
+	do{
+	cout << "Please enter the desired number ";
+	getline(cin, Number)
+	}while(Number>0)
+
+return 0;
+}
