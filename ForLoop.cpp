bc56c01e0b32e76c3880b5dec1e77e9020e9507d
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-18 21:51:16 +0530

    Feat: Add For Loop Code
    Add new For loop code in C++

---
+++ ForLoop.cpp
@@ -0,0 +1,11 @@
+// Following is the code for for loop in C++
+
+int main(){
+	using namespace std;
+	// Following block will list the numbers 1-10 with step of 1
+	// for the step of two, use i+=2
+	for(int i = 1; i <= 10; i++){
+		cout << "man i am loving this....";
+	}
+	return 0;
+}
