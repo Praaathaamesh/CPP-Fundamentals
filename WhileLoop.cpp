99bb0eda6351fa04b2f9f951aa1863a6e724ff6c
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-19 21:03:38 +0530

    Change input method for name from getline to cin
    Fix syntax issues for the while loop issue.

--- WhileLoop.cpp
+++ WhileLoop.cpp
@@ -9,7 +9,7 @@ int main(){
 	// Following while block will execute if the given expression is true
 	while(Name.empty()){
 	cout << "Please enter your name";
-	getline(cin, Name);	
+	cin >> Name;	
 	}
 	return 0;
 }
