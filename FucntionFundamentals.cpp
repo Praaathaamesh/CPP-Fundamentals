c0aa00a4a11901286c8a4dff04f1ee8d871d8f98
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-25 12:08:31 +0530

    Add file fucntion funcdamentals

---
+++ FucntionFundamentals.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Following is the code for understanding the fundamentals of functions
+
+// Creating a custom function as void functionname(){}
+// Keep the fucntion in the same namespace
+// Way one to write the function
+void greet(){
+	std::cout << "hnasdkcans\n";
+	std::cout << "hjsdcvnisbdvj\n";
+	std::cout << "skdbcjwdb\n"; 
+}
+
+int main(){
+
+	greet();
+
+	return 0;
+}
+
