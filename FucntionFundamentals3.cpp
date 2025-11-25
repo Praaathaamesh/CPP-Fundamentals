69089d6f40b71278dc5d81412aa70be8ae45a430
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-25 12:09:21 +0530

    Add file fucntion funcdamentals

---
+++ FucntionFundamentals3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+// Following code talks about the parameters and arguments
+
+void greet(std::string name){ // parameter name string specified
+	std::cout << "Yo" << name;
+}
+
+int main(){
+	std::string Name = "Bro";
+	greet(std::string Name); // argument passed
+	return 0;
+}
