8a6d00677cec0e6a13a29175dad3741f24e543be
Prathamesh Pradeep Jadhav <prathameshpj222@gmail.com>
2025-11-25 12:08:55 +0530

    Add file fucntion funcdamentals

---
+++ FucntionFundamentals2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Way second: call function before then specify the contents
+
+void greet();
+
+int main(){
+	greet();
+	return 0;
+}
+
+void greet(){
+	std::cout << "sdfsjbdskdbj\n";
+	std::cout << "sdcbnsjikbdlfjb\n";
+	std::cout << "jsbkdvsiodbfnsjd\n";
+}
