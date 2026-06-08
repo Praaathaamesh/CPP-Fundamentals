bbb3ccc807fd672ebcbf6b732078605dfd05bd6f
Prathamesh Jadhav <praaathaaamesh@gmail.com>
2026-06-08 15:25:07 +0530

    feat: add getchar static char tuto

---
+++ UserInput_getchar_typecast.cpp
@@ -0,0 +1,29 @@
+// learn about 
+/*
+- char user input
+- ascii association of ints with chars
+- typecast the ints into chars
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Suggest Affirm Key." << std::endl;
+    int usrChar;
+    char parseChar;
+    usrChar = getchar(); // takes single char (same as cin)
+    parseChar = static_cast<char>(usrChar); //static typecast in compile time; not runtime
+    // or use char() for simpler conversions (not recommneded)
+    std::cout << "You put " << parseChar << std::endl;
+
+    if (tolower(parseChar) == 'y')
+    {
+        std::cout << "Unit input accepted.";
+    }
+
+    // system("pause"); custom "press any key to continue" msg
+
+    return 0;
+}
